cb92dfc3c537fc261052786ad920db0b2ad74913
amio-264 <amiosarkar264@gmail.com>
2025-11-18 01:21:38 +0600

    better version

--- 2_linear_search.cpp
+++ 2_linear_search.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int linear_search(int arr[], int n, int data)
+int linear_search(int arr[], int n, int key)
 {
-    for (int i = 0; i < n; i++){
-        if(arr[i]==data){
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
             return i;
-        }
     }
     return -1;
 }
 int main()
 {
-    int arr[] = {10, 20, 35, 45, 67, 89, 98};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int target;
-    cin >> target;
-    int result = linear_search(arr, n, target);
+    int n, key;
+    cout << "Enter the nunmber of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "Enter key to search: ";
+    cin >> key;
+    int result = linear_search(arr, n, key);
     if (result != -1)
-    {
-        cout << "Element found" << endl;
-    }
+        cout << "Element found at index: " << result;
     else
-    {
-        cout << "Element not found" << endl;
-    }
+        cout << "Element not found";
     return 0;
 }
