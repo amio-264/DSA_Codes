26c9a91643d335dde1185070e8a27498e12b1b1a
amio-264 <amiosarkar264@gmail.com>
2025-11-18 01:39:59 +0600

    edit

--- 1_binary_search.cpp
+++ 1_binary_search.cpp
@@ -2,41 +2,35 @@
 using namespace std;
 int binary_search(int arr[], int n, int data)
 {
-    int l, r;
-    l = 0;
-    r = n - 1;
+    int l = 0;
+    int r = n - 1;
     while (l <= r)
     {
         int mid = (l + r) / 2;
         if (data == arr[mid])
-        {
             return mid;
-        }
         else if (data > arr[mid])
-        {
             l = mid + 1;
-        }
         else
-        {
             r = mid - 1;
-        }
     }
     return -1;
 }
 int main()
 {
-    int arr[] = {10, 20, 35, 45, 67, 89, 98};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int target;
-    cin>>target;
-    int result = binary_search(arr, n, target);
+    int n, data;
+    cout << "Enter the number of Elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "Enter the element to find: ";
+    cin >> data;
+    int result = binary_search(arr, n, data);
     if (result != -1)
-    {
-        cout << "Element found" << endl;
-    }
+        cout << "Element is found: " << result;
     else
-    {
-        cout << "Element not found" << endl;
-    }
+        cout << "Element is not found.";
     return 0;
 }
