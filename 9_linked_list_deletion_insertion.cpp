0b73a4f53151c0f8f65b350dee922d0e12140928
amio-264 <amiosarkar264@gmail.com>
2025-12-01 04:03:06 +0600

    new

--- 9_linked_list_deletion_insertion.cpp
+++ 9_linked_list_deletion_insertion.cpp
@@ -5,15 +5,14 @@ class Node
 public:
     int val;
     Node *next;
-    Node *prev;
     Node(int val)
     {
         this->val = val;
         this->next = NULL;
-        this->prev = NULL;
     }
 };
-void print_forward(Node *head)
+
+void print_linked_list(Node *head)
 {
     Node *temp = head;
     while (temp != NULL)
@@ -21,62 +20,9 @@ void print_forward(Node *head)
         cout << temp->val << " ";
         temp = temp->next;
     }
-    cout << endl;
 }
-void delete_head(Node *&head, Node *&tail)
-{
-    Node *deletenode = head;
-    head = head->next;
-    delete deletenode;
-    if (head == NULL)
-    {
-        tail = NULL;
-        return;
-    }
-    head->prev = NULL;
-}
-void delete_tail(Node *&head, Node *&tail)
-{
-    Node *deletenode = tail;
-    tail = tail->prev;
-    delete deletenode;
-    if (tail == NULL)
-    {
-        head = NULL;
-        return;
-    }
-    tail->next = NULL;
-}
-void delete_any_pos(Node *&head, int idx)
-{
-    Node *temp = head;
-    for (int i = 1; i < idx; i++)
-    {
-        temp = temp->next;
-    }
-    Node *deletenode = temp->next;
-    temp->next = temp->next->next;
-    temp->next->prev = temp;
-    delete deletenode;
-}
-void insert_head(Node *&head, int val)
-{
-    Node *newnode = new Node(val);
-    newnode->next = head;
-    head = newnode;
-}
-void insert_any_pos(Node *&head, int index, int val)
-{
-    Node *newnode = new Node(val);
-    Node *temp = head;
-    for (int i = 1; i < index; i++)
-    {
-        temp = temp->next;
-    }
-    newnode->next = temp->next;
-    temp->next = newnode;
-}
-void insert_tail(Node *&head, Node *&tail, int val)
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
 {
     Node *newnode = new Node(val);
     if (head == NULL)
@@ -88,23 +34,78 @@ void insert_tail(Node *&head, Node *&tail, int val)
     tail->next = newnode;
     tail = newnode;
 }
+
+void delete_head(Node *&head)
+{
+    Node *deleteNode = head; // create a node to push the head back
+    head = head->next;       // push the head to next
+    delete deleteNode;       // delete the created node
+}
+
+void delete_any_pos(Node *&head, int index)
+{
+    Node *temp = head;
+    for (int i = 1; i < index; i++)
+    {
+        temp = temp->next;
+    }
+    Node *deleteNode = temp->next;
+    temp->next = temp->next->next;
+    delete deleteNode;
+}
+
+void delete_value(Node *&head, int x)
+{
+    if (head->val == x)
+    {
+        Node *deletenode = head;
+        head = head->next;
+        delete deletenode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        if (tmp->next->val == x)
+        {
+            Node *deletenode = tmp->next;
+            tmp->next = tmp->next->next;
+            delete deletenode;
+            break;
+        }
+        tmp = tmp->next;
+    }
+}
+
+void delete_tail(Node *&head, Node *&tail, int index)
+{
+    Node *temp = head;
+    for (int i = 1; i < index; i++)
+    {
+        temp = temp->next;
+    }
+    Node *deleteNode = head;
+    temp->next = temp->next->next;
+    delete deleteNode;
+    tail = temp;
+}
 int main()
 {
     Node *head = NULL;
     Node *tail = NULL;
+    int x;
     while (1)
     {
-        int x;
         cin >> x;
         if (x == -1)
         {
             break;
         }
-        insert_tail(head, tail, x);
+        insert_at_tail(head, tail, x);
     }
-    print_forward(head);
-    delete_any_pos(head, 1);
-    cout << "After deleting: ";
-    print_forward(head);
+    delete_head(head);
+    // delete_any_pos(head, 2);
+    delete_tail(head, tail, 3);
+    print_linked_list(head);
     return 0;
 }
